7e23759f3f2037ec2d398fc8497c9c758ae2e5a5
AgwaB <leesd556@gmail.com>
2020-03-01 17:11:55 +0900

    add example service

--- example_service_b/src/main/java/com/example/example_service_b/ExampleServiceBApplication.java
+++ example_service_b/src/main/java/com/example/example_service_b/ExampleServiceBApplication.java
@@ -1,4 +1,4 @@
-package com.example.example_service_a;
+package com.example.example_service_b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ExampleServiceAApplication {
+public class ExampleServiceBApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExampleServiceAApplication.class, args);
+        SpringApplication.run(ExampleServiceBApplication.class, args);
     }
 
 }
