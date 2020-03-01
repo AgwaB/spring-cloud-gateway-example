f6f0bb9b48707086d2b9f9a864f0dd3ae56bacd8
AgwaB <leesd556@gmail.com>
2020-03-01 16:52:58 +0900

    init basic eureka service

---
+++ example_service_a/src/main/java/com/example/example_service_a/ExampleServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.example.example_service_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleServiceAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleServiceAApplication.class, args);
+    }
+
+}
---
+++ example_service_a/src/test/java/com/example/example_service_a/ExampleServiceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example_service_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleServiceAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
